cda4912ab3adaba255e33458dfa307130a21cf40
Anton <anton6733@gmail.com>
2024-02-03 16:32:37 +0200

    ready?

--- 4/dz4/website.py
+++ 4/dz4/website.py
@@ -15,6 +15,10 @@
 """
 import os
 import pdb
+import asyncio
+from random import choice
+
+import aiohttp
 from pathlib import Path
 from sys import argv, exit
 import requests
@@ -50,10 +54,24 @@ def time_it(func: Callable):
         for i in args:
             mini_start = time()
             func(i)
-            print(f"{func.__name__} time:{time()-mini_start}")
+            print(f"{func.__name__} time:{time() - mini_start}")
         return True
 
     return wrapper
+
+
+async def __get_one_photo_async(item: str):
+    """декоратор здесь приводит к ошибке ._."""
+    local_start = time()
+    file_name = f"{item.split('/')[-1].split('.')[0]}.jpg"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(item) as response:
+            content = await response.content.read()
+            with open(file=(
+                    STORAGE / file_name).as_posix(),
+                      mode="wb") as file_:
+                file_.write(content)
+    print(f"Photo {file_name} downloaded in {time() - local_start:.6f} seconds.")
 
 
 def __get_one_photo_sync(item: str):
@@ -110,10 +128,23 @@ def get_all_pics(approach: str):
                 threads.append(thread)
             for thread in threads:
                 thread.join()
-        case "3":
-            print("3")
         case _:
+            print(help_on_console())
             raise ValueError("1st arg in [0,1,2,3] !")
+
+
+async def get_all_pics_false_asynced():
+    tasks = []
+    for pic_path in argv[2:]:
+        await __get_one_photo_async(pic_path)
+
+
+async def get_all_pics_asynced():
+    tasks = []
+    for pic_path in argv[2:]:
+        task = asyncio.ensure_future(__get_one_photo_async(pic_path))
+        tasks.append(task)
+    await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
@@ -121,4 +152,14 @@ if __name__ == '__main__':
         print(help_on_console())
         quit()
     clean_up()
-    get_all_pics(argv[1])
+    if argv[1] == "3":
+        start_async = time()
+        strategy = choice((True, False))
+        if strategy:
+            asyncio.run(get_all_pics_false_asynced())  # я так понимаю, что это синхронная загрузка
+        else:
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(get_all_pics_asynced())
+        print(f"Downloaded asynchronously in {time() - start_async:.6f} seconds.")
+    else:
+        get_all_pics(argv[1])
