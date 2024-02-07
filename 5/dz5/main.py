3557ea0f18397a52f6a30327250318ef4a3186e3
Anton <anton6733@gmail.com>
2024-02-07 02:32:21 +0200

    prg

--- 5/dz5/main.py
+++ 5/dz5/main.py
@@ -13,3 +13,42 @@ API должен содержать следующие конечные точк
 Для каждой конечной точки необходимо проводить валидацию данных запроса и ответа.
 Для этого использовать библиотеку Pydantic.
 """
+from pydantic import BaseModel
+from fastapi import FastAPI, Response
+from typing import Optional
+
+app = FastAPI()
+
+
+class Task(BaseModel):
+    title: str
+    todo: Optional[str]
+    status: Optional[bool]
+
+
+class Base_of_sorts(dict):
+    def put(self):
+
+@app.get("/tasks/")
+async def select_all():
+    raise NotImplemented
+
+
+@app.get("/tasks/{task_id}")
+async def select_one():
+    raise NotImplemented
+
+
+@app.post("/tasks/")
+async def post_task():
+    raise NotImplemented
+
+
+@app.put("/tasks/{task_id}")
+def edit_task():
+    raise NotImplemented
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task():
+    raise NotImplemented
